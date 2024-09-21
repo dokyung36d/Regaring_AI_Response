d5ecd5d2ac84e76d33a7be4e0c3c0263997b8982
dokyu <dokyung36@naver.com>
2024-09-21 22:52:53 +0900

    openai embedding pipeline finish

--- test_openai.py
+++ test_openai.py
@@ -7,15 +7,26 @@ client = OpenAI(
   api_key=openai_key
 )
 
-stream = client.chat.completions.create(
-    model="gpt-4o",
-    messages=[{"role": "user", "content": "Hi my name is dokyung"},
-              {"role": "assistant", "content": "Hi Dokyung! How can I assist you today?"},
-              {"role": "user", "content": "do you remember my name?"},
-              {"role": "assistant", "content": "Hi Dokyung! How can I assist you today?"},
-              {"role": "user", "content": "can you tell me about hyunjin-ryu?"}],
-    stream=True,
+# stream = client.chat.completions.create(
+#     model="gpt-4o",
+#     messages=[{"role": "user", "content": "Hi my name is dokyung"},
+#               {"role": "assistant", "content": "Hi Dokyung! How can I assist you today?"},
+#               {"role": "user", "content": "do you remember my name?"},
+#               {"role": "assistant", "content": "Hi Dokyung! How can I assist you today?"},
+#               {"role": "user", "content": "can you tell me about hyunjin-ryu?"}],
+#     stream=True,
+# )
+
+# for chunk in stream:
+#     if chunk.choices[0].delta.content is not None:
+#         print(chunk.choices[0].delta.content, end="")
+
+responese = client.embeddings.create(
+  model="text-embedding-ada-002",
+  input="Turkish Food is Delicious!!",
+  encoding_format="float"
 )
-for chunk in stream:
-    if chunk.choices[0].delta.content is not None:
-        print(chunk.choices[0].delta.content, end="")
+
+print("hello world")
+
+print(responese.data[0].embedding)
