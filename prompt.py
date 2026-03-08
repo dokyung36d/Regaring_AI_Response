2747d3b166e58cffe4b380f54b2b63d5adc646a2
윤도경 <youndokyung@yundogyeong-ui-Macmini.local>
2026-03-08 17:19:15 +0900

    fix: remove redundant embedding call in get_relevant_newspapers

--- prompt.py
+++ prompt.py
@@ -1,6 +1,5 @@
 import json
 import random
-from embedding import get_embedding
 from fetch_relevant import fetch_relevant_document
 from openai import OpenAI
 import os
@@ -11,7 +10,6 @@ from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 
 def get_relevant_newspapers(hobby, newspaper_title):
     prompt = f"User's hobby is {hobby} and he is reading newspaper which name is {newspaper_title}"
-    prompt_embedding = get_embedding(prompt)
 
     fetched_newspapers = fetch_relevant_document(prompt, database="newspaperTitle", collection="title", num_fetched=5, index_name="title_vector_index")
 
